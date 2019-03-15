b489eb1b1adb57b065792dcc70c5d0e382266a28
Andrei Goncharov <ravingandrey@gmail.com>
2019-03-15 18:57:46 +0300

    Initial Commit

---
+++ CityGuess/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  CityGuess
+//
+//  Created by Андрей Гончаров on 15/03/2019.
+//  Copyright © 2019 Андрей Гончаров. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
